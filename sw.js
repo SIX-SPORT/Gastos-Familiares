6f7ed97a5399e2a3051e5f197e97a52b43543d49
SIX-SPORT <oscar-zuca@hotmail.com>
2026-07-05 19:40:54 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="gastos-familia-v8";
+const CACHE="gastos-familia-v9";
 const SHELL=["./index.html","./config.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).catch(()=>{})); });
 self.addEventListener("activate",e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
